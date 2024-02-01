d2634a21f17392c5c33a247b6a736c6a72784c6a
yousefhindi1 <yousefhindi8422@gmail.com>
2024-01-31 19:19:41 -0500

    Final Commit

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
 const form = document.querySelector('#form')
 const submitButton = document.querySelector('#submit')
 
-
-form.addEventListener('Submit', (e) => {
-console.log("Success!")
+form.addEventListener('submit', (e) => {
+  console.log("hi")
   submitButton.disabled = true
   e.preventDefault()
-  window.location.href = window.location.origin + '/success.html'
+  window.location.href = 'success.html'
 })
 
--- success.js
+++ success.js
@@ -1,3 +1,3 @@
 setTimeout(() => {
-    window.location.href = window.location.origin
+    window.location.href = "index.html"
   }, 5000)
